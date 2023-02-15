b07ca8cbb14c7631b5b3190daa463f43f275d1ac
Ariadne Engelbrecht <ariadneengelbrecht@dionysos.local>
2023-02-15 17:57:32 +0100

    Create web component for transforming icon with dropdown.

--- components/burger_with_dropdown.js
+++ components/burger_with_dropdown.js
@@ -1,4 +1,4 @@
-class burgerMenu extends HTMLElement {
+class burgerWithDropdown extends HTMLElement {
   connectedCallback() {
 
     const shadowRoot = this.attachShadow({ mode: 'open' });
@@ -43,6 +43,24 @@ class burgerMenu extends HTMLElement {
       .burger-menu.active .line-3 {
         transform: translateY(-0.5rem) rotate(-50deg);
       }
+
+
+      menu {
+        display: none;
+      }
+
+      menu.active{
+        display: block;
+        /* width: -moz-fit-content;
+        width: fit-content; */
+        width: calc(100vh*0.25);
+      }
+
+      menu li{
+        list-style: none;
+        padding: 0.4rem;
+        border-bottom: 1px solid #6c6c6c;
+      }
     </style>
 
     <div class="burger-menu">
@@ -50,12 +68,17 @@ class burgerMenu extends HTMLElement {
       <span class="line line-2"></span>
       <span class="line line-3"></span>
     </div>
-
+    <menu>
+      <li><a href="#">Home</a></li>
+      <li><a href="#">About</a></li>
+      <li><a href="#">Contact</a></li>
+    </menu>
 `
     this.onclick = function () {
       shadowRoot.querySelector('.burger-menu').classList.toggle('active');
+      shadowRoot.querySelector('menu').classList.toggle('active');
     }
   }
 }
 
-customElements.define('burger-menu', burgerMenu);
+customElements.define('burger-with-dropdown', burgerWithDropdown);
