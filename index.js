4cecfafeddbd0afeda1dfa64c2a407f553b67a06
Ariadne Engelbrecht <ariadneengelbrecht@dionysos.local>
2023-02-15 15:40:08 +0100

    Add dropdown menu.

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 function toggleMenu() {
   const burgerMenu = document.querySelector('.burger-menu');
   burgerMenu.classList.toggle('active');
+
+  const menuItems = document.getElementsByTagName('menu')[0];
+  menuItems.classList.toggle('active');
 }
